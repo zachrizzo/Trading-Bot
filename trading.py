8e6616330c17d70dba5d346b0e65bfa9290d3782
zachrizzo <zachcilwa@gmail.com>
2023-03-15 18:25:40 -0400

    first commit

---
+++ trading.py
@@ -0,0 +1,70 @@
+import time
+# import nest_asyncio
+# from alpaca.data.live import StockDataStream
+# from alpaca.data.historical import StockHistoricalDataClient
+# from alpaca.trading.requests import MarketOrderRequest
+# from alpaca.trading.enums import OrderSide, TimeInForce
+# from alpaca.trading.client import TradingClient
+# from alpaca.trading.requests import GetAssetsRequest
+# from alpaca.trading.enums import AssetClass
+# #trading_client
+# from alpaca.trading.client import TradingClient
+# #LimitOrderRequest
+# from alpaca.trading.requests import LimitOrderRequest
+
+# from alpaca.data.requests import StockLatestQuoteRequest
+
+
+# nest_asyncio.apply()
+import alpaca_trade_api as tradeapi
+import pandas as pd
+
+
+API_KEY = "PKUM529GXGRDHTQHPPI8"
+SECRET_KEY = "hUV2Q6TgSVOrdl9D7r0nNoPMYlgN6Mr1HpOnX0Pl"
+
+api_key = API_KEY
+api_secret = SECRET_KEY
+base_url = 'https://paper-api.alpaca.markets'  # Replace with 'https://api.alpaca.markets' for live trading
+
+api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
+
+for i in range(0, 10):
+    time.sleep(3)
+    crypto_data = api.get_latest_crypto_trades(symbols=['BTC/USD'])
+
+    print(crypto_data)
+
+# df_spy = pd.DataFrame({
+
+#     'price': [spy_data.p],
+#     'timestamp': [spy_data.t]
+# })
+
+# print(df_spy)
+
+
+
+# wss_client = StockDataStream(API_KEY, SECRET_KEY ,raw_data=True) #url_override="wss://stream.data.markets/v2/iex")
+# trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+# historical_data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
+
+
+
+# # Getting account information and printing it
+# account = trading_client.get_account()
+# for property_name, value in account:
+#   print(f"\"{property_name}\": {value}")
+
+
+
+
+# #url_override="wss://stream.data.markets/v2/iex"
+# async def quote_data_handler(data):
+#     time.sleep(3)
+
+#     print(data)
+
+# wss_client.subscribe_bars(quote_data_handler, "AAPL")
+
+# wss_client.run()
